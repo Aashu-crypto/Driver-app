81886291396ced4f590cfad38610032e2b407fd7
Aashu-Crypto <ashugandotra14@gmail.com>
2024-08-21 23:38:29 +0530

    setup

---
+++ GlobalStyles.js
@@ -0,0 +1,25 @@
+import { Dimensions } from "react-native";
+
+/* screen dimension  */
+
+export const { width, height } = Dimensions.get("window");
+
+/* fonts */
+
+export const FontFamily = {
+  poppinsBold: "Poppins-Bold",
+  poppinsRegular: "Poppins-Regular",
+};
+
+export const Color = {
+  colorGainsboro: "#e3e3e3",
+  colorDarkgray: "#9b9b9b",
+  colorDarkslategray: "#363636",
+  colorGray_100: "#848484",
+  colorSilver: "#c4c4c4",
+  colorGray: "#828282",
+  appDefaultColor: "#1C4BF4",
+  secondaryColor: "#102C8E",
+  colorGrayLight: "#C4C4C4",
+  colorGrayNormal: "#4C4E64",
+};
